bd8165585f61012e4192e0b7f2b144797e952e50
mac <lironghua@govion.cn>
2022-05-11 11:23:22 +0800

    项目配置

---
+++ src/main/java/com/example/mybatis/config/JiangsuConfig.java
@@ -0,0 +1,16 @@
+package com.example.mybatis.config;
+
+import com.example.mybatis.utils.GeneratorUtils;
+
+/**
+ * @ClassName JiangsuConfig
+ * @Description TODO
+ * @Author mac
+ * @DATE 2022/5/11 11:11
+ */
+public class JiangsuConfig {
+    public static void main(String[] args) {
+        GeneratorUtils generatorUtils = new GeneratorUtils();
+        generatorUtils.init("jiangsu");
+    }
+}
